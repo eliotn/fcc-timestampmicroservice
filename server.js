7148d09609e040b882d5742992fa00e021d0e4f6
Eliot Glairon <ejg_dnd@yahoo.com>
2016-10-02 12:57:07 +0000

    Final changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var http = require('http');
 var url = require('url');
 var moment = require('moment');
+var express = require('express');
 moment().format();
 var server = http.createServer(function (req, res) {
     var requestdata = url.parse(req.url, true);
@@ -9,9 +10,21 @@ var server = http.createServer(function (req, res) {
     var urlpath = decodeURIComponent(requestdata.pathname);
     var date, unixtime, naturaldate, momentparse;
     if (urlpath.match(/^\/$/)) {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.end("Welcome to the timestamp microservice.  Provide a date, and \
-        the microservice will parse the date");
+        res.writeHead(200, {'content-type': 'text/html'});
+        var todaysmoment = moment(moment().format("MMMM D, Y"), "MMMM D, Y", 'en', true);
+        var todaysmomentformatted = todaysmoment.format("MMMM D, Y");
+        var todaysunixtime = todaysmoment.unix();
+        res.end("<html><head><title>Timestamp Microservice</title></head><body>" +
+        "<h1>Welcome to Eliot's timestamp microservice.</h1><h3>To use this serveice, put " +
+        "a unix timestamp in the format " +
+        "/[integer timestamp]' or a date in the format " +
+        "/[Month Name]%20[Day],%20[Year]'.</a></h3>" +
+        "<p>For example if you request <a href='./" + todaysmomentformatted + "'>" +
+        "'/" + encodeURI(todaysmomentformatted) + "'</a> " + 
+        "or  <a href='./" + encodeURI(todaysmomentformatted) + "'>'/" + todaysunixtime + "'</a>" +
+        "</p><p>You will get the following output: {" +
+        "\"unixtime\":" + todaysunixtime + ",\"natural\":\"" + todaysmomentformatted + "\"}" +
+        "</body></html>");
     }
     res.writeHead(200, {'Content-Type': 'application/json'});
     if (urlpath.match(/^\/-{0,1}[0-9]+$/)) {
@@ -19,24 +32,15 @@ var server = http.createServer(function (req, res) {
         
     }
     else {
-        momentparse = moment(urlpath.slice(1), "MMMM DD, Y", 'en', true);
+        momentparse = moment(urlpath.slice(1), "MMMM D, Y", 'en', true);
     }
     if (!momentparse.isValid()) {
-        res.end(JSON.stringify({"error":"Invalid input for the date."}));
+        res.end(JSON.stringify({"unixtime":null,"natural":null}));
     }
     unixtime = momentparse.unix();
-    naturaldate = momentparse.format("MMMM DD, Y");
+    naturaldate = momentparse.format("MMMM D, Y");
         
     res.end(JSON.stringify({"unixtime":unixtime,"natural":naturaldate}));
-        /*
-    else if (requestdata["pathname"] === "/api/parsetime") {
-        res.end(JSON.stringify({"hour":date.getHours(), "minute":date.getMinutes(), "second":date.getSeconds()}));
-        
-    }
-    else {
-        res.writeHead(404)
-        res.end()
-    }*/
     
     
     
